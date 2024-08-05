eb2eed9a69b07626310d920d932b81751913eda9
RB <rkrb@tyfonmail.se>
2024-08-05 17:04:34 +0200

    test data in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import {TodoList} from "./components/TodoList.tsx";
+import {ITodoNote} from "./interfaces.ts";
+
 export function App() {
-  return (
-    <>
-      <h1>This is the App component</h1>
-    </>
-  );
+
+    const notes: ITodoNote[] = [
+        {
+            date: new Date('2024-08-05'),
+            author: 'John Doe',
+            name: 'Learn TypeScript',
+            description: 'Master the basics of TypeScript.',
+            completed: false,
+        },
+        {
+            date: new Date('2024-08-06'),
+            author: 'Jane Smith',
+            name: 'Finish Project X',
+            description: 'Complete the final stages of Project X.',
+            completed: true,
+        },
+    ];
+    return (
+        <>
+            <TodoList todoList={notes}/>
+        </>
+    );
 }
