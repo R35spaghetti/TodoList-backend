17afb1c37292079debfec0c50553af7a41118885
rb <rkrb@tyfonmail.se>
2024-08-05 22:38:46 +0200

    function to delete, based on string for now

--- src/data.ts
+++ src/data.ts
@@ -3,3 +3,7 @@ import {ITodoNote} from "./interfaces.ts";
 export function AddTodo(collection: ITodoNote[], todoNote: ITodoNote): ITodoNote[] {
     return [...collection, todoNote]
 }
+
+export function DeleteNoteByName(todoNotes: ITodoNote[], index: string): ITodoNote[] {
+    return todoNotes.filter(todoNotes => todoNotes.name !== index);
+}
