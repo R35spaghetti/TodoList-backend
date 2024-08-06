12deafc84beefeaa07decc55807e666e7420340d
rb <rkrb@tyfonmail.se>
2024-08-06 16:12:30 +0200

    implemented todolist into router

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,12 @@ import {createBrowserRouter, createRoutesFromElements, Route} from "react-router
 import {AddTodoNote} from "./components/AddTodoNote.tsx";
 import About from "./components/About.tsx";
 import {App} from "./App.tsx";
+import {TodoList} from "./components/TodoList.tsx";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App/>}>
+            <Route index element={<TodoList/>}/>
             <Route path="form" element={<AddTodoNote/>}/>
             <Route path="about" element={<About />} />
         </Route>
