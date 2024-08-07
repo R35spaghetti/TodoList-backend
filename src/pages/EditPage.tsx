9b46f219ff9f040cb021dac72826f9ea2df81bb4
RB <rkrb@tyfonmail.se>
2024-08-07 02:05:07 +0200

    Edit page implemented

---
+++ src/pages/EditPage.tsx
@@ -0,0 +1,55 @@
+import React, {FormEventHandler, ReactElement, useState} from "react";
+import {useTodoNoteContext} from "../hooks/useTodoNoteContext.ts";
+import {ITodoNote} from "../interfaces.ts";
+import {Input} from "../components/Input.tsx";
+import {useNavigate, useParams} from "react-router-dom";
+
+export function EditPage() : ReactElement
+{
+    const { index } = useParams();
+    const safeIndex = index || '0';
+    const {todoNotes, onUpdate} =  useTodoNoteContext();
+    const noteToUpdate = todoNotes[parseInt(safeIndex)];
+    const navigate = useNavigate();
+
+
+    const [updateTodoNote, setUpdateTodoNote] = useState<ITodoNote>(
+        {
+            date: noteToUpdate.date,
+            author: noteToUpdate.author,
+            name: noteToUpdate.name,
+            description: noteToUpdate.description,
+            completed: noteToUpdate.completed,
+        }
+    )
+
+    const handleOnSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        onUpdate(updateTodoNote);
+        navigate("/");
+
+    }
+    return (
+        <form className="edit-toDoNote" onSubmit={handleOnSubmit}>
+            <Input label="date"
+                   onChange={(e) => setUpdateTodoNote(prevState => ({...prevState, date: new Date(e.target.value)}))}
+                   type="date" value={updateTodoNote.date}/>
+            <Input label="author"
+                   onChange={(e) => setUpdateTodoNote(prevState => ({...prevState, author: e.target.value}))} type="text"
+                   value={updateTodoNote.author}/>
+            <Input label="name"
+                   onChange={(e) => setUpdateTodoNote(prevState => ({...prevState, name: e.target.value}))}
+                   type="text" value={updateTodoNote.name}/>
+            <Input label="description"
+                   onChange={(e) => setUpdateTodoNote(prevState => ({...prevState, description: e.target.value}))}
+                   type="textarea" value={updateTodoNote.description}/>
+            <Input label="completed"
+                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUpdateTodoNote(prevState => ({
+                       ...prevState,
+                       completed: e.target.checked
+                   }))}
+                   type="checkbox" value={updateTodoNote.completed}/>
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
