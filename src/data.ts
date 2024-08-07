c9d48d3e0457f9a857512eb5f779f08171de8b28
RB <rkrb@tyfonmail.se>
2024-08-07 02:09:48 +0200

    update function actually updates

--- src/data.ts
+++ src/data.ts
@@ -8,10 +8,12 @@ export function DeleteNoteByName(todoNotes: ITodoNote[], index: string): ITodoNo
     return todoNotes.filter(todoNotes => todoNotes.name !== index);
 }
 
-export function UpdateSpecificTodoNote(notes: ITodoNote[], noteToUpdate: ITodoNote):ITodoNote[] {
+export function UpdateSpecificTodoNote(notes: ITodoNote[], noteToUpdate: ITodoNote): ITodoNote[] {
     const index = notes.findIndex(note => note.name === noteToUpdate.name);
     if (index > -1) {
-        return [...notes.slice(0, index), ...notes.slice(index + 1)];
+        const updatedNotes = [...notes];
+        updatedNotes[index] = noteToUpdate;
+        return updatedNotes;
     }
     return notes;
 }
