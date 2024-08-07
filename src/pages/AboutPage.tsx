06d61640871c65ab83c9acd3060c2a315676c10d
RB <rkrb@tyfonmail.se>
2024-08-07 17:39:59 +0200

    about page counts amount of to-do-notes

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,9 +1,14 @@
+import {useTodoNoteContext} from "../hooks/useTodoNoteContext.ts";
+
 export default function AboutPage() {
+
+    const {todoNotes} = useTodoNoteContext();
+
     return (
         <div>
             <h1>About Us</h1>
             <p>This is an about page</p>
+            <p>Amount of notes: {todoNotes.length}</p>
         </div>
     );
 }
-
