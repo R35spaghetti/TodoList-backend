fb50d39a7a34051bc8b42e207e9db838e97d3b91
rb <rkrb@tyfonmail.se>
2024-08-08 11:40:45 +0200

    css added, updated HTML on to-do-list-note page

--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -3,27 +3,28 @@ import {TodoNote} from "../components/TodoNote.tsx";
 import {useTodoNoteContext} from "../hooks/useTodoNoteContext.ts";
 import {Link} from "react-router-dom";
 import {SortsNotes} from "../components/SortsNotes.tsx";
-
+import "./TodoListPage.css";
 
 export function TodoListPage(): ReactElement {
     const {todoNotes, onDelete, onToggleCompleted, onUpdateNeighbours} = useTodoNoteContext();
 
     return (
-        <section className="todo-list">
-            <div>
-                <SortsNotes/>
-            </div>
-            {todoNotes.map((item, index) => (
-                <div key={item.id}>
-                    <TodoNote todoNote={item}/>
-                    <button onClick={() => onDelete(item.id)}>Delete</button>
-                    <input type="checkbox" id="completed" checked={item.completed}
-                           onChange={() => onToggleCompleted(item.name)}/>COMPLETED
-                    <Link to={`/edit/${index}`}>EDIT</Link>
-                    <button onClick={() => onUpdateNeighbours(index, index - 1)}>UPP ARROW</button>
-                    <button onClick={() => onUpdateNeighbours(index, index + 1)}>DOWN ARROW</button>
-                </div>
-            ))}
+        <section>
+            <SortsNotes/>
+            <section className="todo-list">
+                {todoNotes.map((item, index) => (
+                    <div key={item.id}>
+                        <TodoNote todoNote={item}/>
+                        <button onClick={() => onDelete(item.id)}>Delete</button>
+                        <input type="checkbox" id="completed" checked={item.completed}
+                               onChange={() => onToggleCompleted(item.name)}/>COMPLETED
+                        <Link to={`/edit/${index}`}>EDIT</Link>
+                        <button onClick={() => onUpdateNeighbours(index, index - 1)}>UPP ARROW</button>
+                        <button onClick={() => onUpdateNeighbours(index, index + 1)}>DOWN ARROW</button>
+                    </div>
+                ))}
+            </section>
         </section>
+
     )
 }
