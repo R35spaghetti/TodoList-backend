c0aed2510c77567d422d52cfab58d526988f267f
rb <rkrb@tyfonmail.se>
2024-08-08 12:58:27 +0200

    proper info on the about page, added css

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,14 +1,14 @@
 import {useTodoNoteContext} from "../hooks/useTodoNoteContext.ts";
-
+import "./AboutPage.css";
 export default function AboutPage() {
 
     const {todoNotes} = useTodoNoteContext();
 
     return (
-        <div>
-            <h1>About Us</h1>
-            <p>This is an about page</p>
-            <p>Amount of notes: {todoNotes.length}</p>
-        </div>
+        <article className="about-page-container">
+            <h1>About the to-do list</h1>
+            <p>This is a collection of tasks that needs to be completed (according to you).<br/> The list can be organized based on author or date</p>
+            <p className="notes-counter">Amount of notes: {todoNotes.length}</p>
+        </article>
     );
 }
